6eb654a214f6c961802e20e87e666834ebfbe595
Vladimir <scherbinin.vladimir@gmail.com>
2020-03-16 14:30:44 +0300

    выводит список объявлений из mongodb

--- app.py
+++ app.py
@@ -1,30 +1,41 @@
 # Flask 
 #   https://habr.com/ru/post/246699/
 
-adverts = [
-    {
-        'id': 1,
-        'title': 'Дом',
-        'date': '6/Mar/2020',
-        'message': 'Продам дом в деревне',
-        'author': 'Иван Иванов'
-    },
-    {
-        'id': 2,
-        'title': 'Квартира',
-        'date': '16/Mar/2020',
-        'message': 'Продам квартиру в Раменском',
-        'author': 'Петр Петров'
-    },    
-]
+# adverts = [
+#     {
+#         'id': 1,
+#         'title': 'Дом',
+#         'date': '6/Mar/2020',
+#         'message': 'Продам дом в деревне',
+#         'author': 'Иван Иванов'
+#     },
+#     {
+#         'id': 2,
+#         'title': 'Квартира',
+#         'date': '16/Mar/2020',
+#         'message': 'Продам квартиру в Раменском',
+#         'author': 'Петр Петров'
+#     },    
+# ]
 
 import datetime
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, abort, make_response, request, jsonify
+from pymongo import MongoClient
+
+mongo_client = MongoClient('192.168.0.6')
+db = mongo_client.e7db
 
 app = Flask(__name__)
 
+def encode(d):
+    if "_id" in d:
+        d["_id"] = str(d["_id"])
+    return d
+
 @app.route('/bboard/adverts', methods=['GET'])
 def get_adverts():
+    adverts = [a for a in db.adverts.find()]    
+    adverts = list(map(encode, adverts))
     return jsonify({'adverts': adverts})
 
 @app.route('/bboard/adverts/<int:advert_id>')
@@ -39,28 +50,28 @@ def create_advert():
     if not request.json or not 'title' in request.json:
         abort(400)
     advert = {
-        'id': adverts[-1]['id'] + 1,
         'title': request.json['title'],
         'message': request.json.get('message', ""),
         'author': request.json.get('author', ""),
         'date': str(datetime.datetime.now())
     }
-    adverts.append(advert)
-    return jsonify({'advert': advert}), 201
+    db.adverts.insert(advert)
+    print(advert)
+    return "advert"
+    # return jsonify({'advert': advert}), 201
+
+
+
+    
 
 
 @app.route('/bboard/adverts/<int:advert_id>', methods=['PUT'])
 def update_task(advert_id):
     advert = list(filter(lambda t: t['id'] == advert_id, adverts))
-    print("aldkjalkdfjlakdjlak " + str(type(request.json['title'])))
     if len(advert) == 0:
         abort(404)
     if not request.json:
         abort(400)
-    # if 'title' in request.json and type(request.json['title']) != unicode:
-    #     abort(400)
-    # if 'description' in request.json and type(request.json['description']) is not unicode:
-    #     abort(400)
     advert[0]['title'] = request.json.get('title', advert[0]['title'])
     advert[0]['message'] = request.json.get('message', advert[0]['message'])
     return jsonify({'advert': advert[0]})
